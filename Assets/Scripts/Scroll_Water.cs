e53547ed9a64bd5b5dd67e5c0bb6ff6960c216d2
taamfp <ta.am97fp@gmail.com>
2020-08-18 12:35:00 +0100

    Commit

---
+++ Assets/Scripts/Scroll_Water.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll_Water : MonoBehaviour
+{
+    public float scrollX = 0.5f;
+    public float scrollZ = 0.5f;
+    public float timeDifx;
+    public float timeDifz;
+
+    void Update()
+    {
+        timeDifx = Time.time*scrollX;
+        timeDifz = Time.time*scrollZ;
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(timeDifx, timeDifz);
+    }
+}
